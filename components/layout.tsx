1fe980e712ad0891af890f7b12fcfc06d38c95b0
SHJZ <52501040+HamzaZaidiX@users.noreply.github.com>
2022-07-20 20:33:31 +0500

    Add files via upload

---
+++ components/layout.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from 'react'
+
+interface LayoutProps {
+  children?: ReactNode
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex flex-col h-screen justify-between font-content antialiased">    
+      <main className="mb-auto">{children}</main>    
+    </div>
+  )
+}
+
+export default Layout
